38791ca441350a0563ee9e885d67068ad8d8b065
龙龙 <51738561+08183080@users.noreply.github.com>
2023-09-27 19:58:35 +0800

    自定义pcap2csv

--- net-pre/trys/pcap2csv.py
+++ net-pre/trys/pcap2csv.py
@@ -11,6 +11,7 @@ scapy, 包解析, powerful
 
 from scapy.all import *
 import pyshark
+import csv
 
 """
 从path路径下读取pcap文件
@@ -26,28 +27,49 @@ first you have to reach the appropriate layer and then you can select your field
 
 """
 def read(path, filter):
-    packets = pyshark.FileCapture(path, filter)
+    packets = pyshark.FileCapture(path, display_filter = filter)
     return packets
 
 """
 将packet进行解析...
 """
 def parse_row(packet):
+    list = []
     no = packet.number
-    src = packet['ip'].src
-    dst = packet['ip'].dst
-    print(no)
-    print(src, "->", dst)
-    packet.ftp.pretty_print()
+    src = packet.ip.src
+    dst = packet.ip.dst
+    print("no: ",no)
+    print("src: ",src)
+    print("dst: ",dst)
+    # packet.ftp.pretty_print()
     # print(content)
-    
+    """若是ftp协议
+    我目前只对于请求序列感兴趣,只提取request部分...  9/27
+    """
+    if packet.ftp != None:
+        proto = "ftp"
+        print("proto: ftp")
+        if packet.ftp.request_command != None:
+            ftp_request = packet.ftp.request_command 
+            if packet.ftp.request_arg != None:
+               ftp_request = ftp_request + " " + packet.ftp.request_arg
+               print("request: ",ftp_request)
+               list.append(no)
+               list.append(src)
+               list.append(dst)
+               list.append(proto)
+               list.append(ftp_request)
+    return list
+               
 """
 发现pyshark项目:
 没有找到项目里面如何获得time,如何获得info内容?
 找不到类里面定义的内容的话,我就暴力提取
+
+A: debug的时候,发现一些请求消息的一些属性的端倪...
 """
 
-
+lists = []
 path = "D:\\pre-boofuzz\\net-pre\\data\\6.pcap"
 filter = "ftp"
 packets = read(path, filter) 
@@ -55,9 +77,21 @@ packets = read(path, filter)
 for packet in packets:
     # parse_row(packet)
     # print(packet)
-    parse_row(packet)
-    break
+    try:
+        list = parse_row(packet)
+        lists.append(list)
+    except Exception as e:
+        pass
+        continue
+
+header = ['no','src','dst','proto','ftp_request']
+# print(lists)
+with open("./ans.csv", "a+", encoding="utf-8", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(lists)
+
     
-
-
-
+"""
+初步实现把自定义提取的信息写入csv文件
+"""
