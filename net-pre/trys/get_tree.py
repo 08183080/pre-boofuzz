efab6fd790d1d471b39b13f10bbf58767525844e
dragon <51738561+08183080@users.noreply.github.com>
2023-11-02 15:22:04 +0800

    【迟到1个月的进展】多序列合并形成协议树

--- net-pre/trys/get_tree.py
+++ net-pre/trys/get_tree.py
@@ -1,44 +1,62 @@
-"""
-i want to get a tree
-"""
+import networkx as nx
+import matplotlib.pyplot as plt
 
-s1 = [1, 0, 2]
-s2 = [1, 0, 3]
-s3 = [1, 0, 4, 5]
+"""
+成功实现多序列合并实现序列树
+"""
 
 class TreeNode:
     def __init__(self, value) -> None:
         self.value = value
-        self.n1 = None
-        self.n2 = None
-        self.n3 = None
+        self.children = []
 
+def add(self, l: list):
+    n = len(l)
+    current_node = self
 
-    def add(self, l: list):
-        n = len(l)
-        # self.value = l[0]
-        for i in range(1, n):
-            if self.n1 and self.n1.value == l[i]:
+    for i in range(1, n):
+        found = False
+        for child in current_node.children:
+            if child.value == l[i]:
+                current_node = child
+                found = True
                 break
-            elif self.n2 and self.n2.value == l[i]:
-                break
-            elif self.n3 and self.n3.value == l[i]:
-                break
-            elif self.n1 == None:
-                self.n1.value = l[i]
-            elif self.n1 and self.n2 == None:
-                self.n2.value = l[i]
-            else:
-                self.n3.value = l[i]         
 
+        if not found:
+            new_node = TreeNode(l[i])
+            current_node.children.append(new_node)
+            current_node = new_node
+
+    return self
+
+def visualize_protocol_tree(root):
+    G = nx.Graph()
+
+    def traverse(node, parent=None):
+        G.add_node(node.value)
+
+        if parent:
+            G.add_edge(parent.value, node.value)
+
+        for child in node.children:
+            traverse(child, node)
+
+    traverse(root)
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1500, font_size=10, font_weight='bold', edge_color='gray', width=1.5)
+    plt.show()
 
 if __name__ == "__main__":
     t = TreeNode(1)
-    t.add(s1)
-    # t.n1 = TreeNode(0)
-    # t.n1.n1 = TreeNode(2)
-    # t.n1.n2 = TreeNode(3)
-    # t.n1.n3 = TreeNode(4)
-    # t.n1.n3.n1 = TreeNode(5)
-    
+    s1 = [1, 0, 2]
+    s2 = [1, 0, 3]
+    s3 = [1, 0, 4, 5]
+    s4 = [1, 6]
+    # s5 = [1, 7, 9, 2]
+    t = add(t, s1)
+    t = add(t, s2)
+    t = add(t, s3)
+    t = add(t, s4)
+    # t = add(t, s5)
 
+    visualize_protocol_tree(t)
